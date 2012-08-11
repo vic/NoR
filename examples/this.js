d1e57d1e74ce88d8439a58290fd0f2ef982f87bc
Victor Borja <vborja@apache.org>
2012-08-11 09:07:04 -0500

    Added example on how to bind this to a cell.

---
+++ examples/this.js
@@ -0,0 +1,22 @@
+var NoR = require('../lib/NoR.js');
+
+var Person = function(name) {
+  this.name = name
+  this.hello = function(you){
+    return "Hello "+you.name+" my name is "+this.name
+  }
+}
+
+var victor = new Person("Victor")
+var hugo = new Person("Hugo")
+
+var hello = NoR(function(you){
+  console.log(this.hello(you()))
+}, null, victor)
+
+hello(hugo)
+
+setTimeout(function(){
+  hello.self(hugo)
+  hello(victor)
+}, 10)

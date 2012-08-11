e45c6d37fd56681115bff4a4c782621ade4517a7
Victor Borja <vborja@apache.org>
2012-08-10 19:52:14 -0500

    Added a simple number arithmetic example.

---
+++ examples/add.js
@@ -0,0 +1,21 @@
+var NoR = require('../lib/NoR.js');
+
+var add = NoR(function(a, b, c){
+  c( a() + b() )
+})
+
+add.c.subscribe(function(c){ console.log(add.toString()) })
+
+add(1, 2) // should output 3
+
+setTimeout(function(){
+  add.a(2) // should output a 4
+}, 100)
+
+setTimeout(function(){
+  add.b(5) // should output a 7
+}, 200)
+
+setTimeout(function(){
+  add(12, 12) // should output the answer to the universe life and everything
+}, 300)

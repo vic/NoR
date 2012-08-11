0b061bc675030fef28391ea42eb8dd5839b77e12
Victor Borja <vborja@apache.org>
2012-08-10 21:47:13 -0500

    Added identity example

---
+++ examples/identity.js
@@ -0,0 +1,8 @@
+var NoR = require('../lib/NoR.js');
+
+var identity = NoR(function(a, b){}, function(a, b){ a.bind(b) })
+
+identity.b.subscribe(function(x) { console.log("VALUE IS ", x) })
+
+identity(1)
+identity(22)
